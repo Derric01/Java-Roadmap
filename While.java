a07dc751f63f865a7915eb5caa33e9b21ef2f624
Derric samson <140136414+Derric01@users.noreply.github.com>
2024-12-11 22:05:40 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class While {
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int i = obj.nextInt();
+       // while(i<10){
+          //  System.out.println("********");
+        //}
+        do { 
+           System.out.println("odd looks");
+        }while(i==8);
+        
+        obj.close();
+    }
+}
