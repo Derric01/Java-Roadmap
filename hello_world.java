c9e9810e7b50d4464405c28db7c0032f501c4c00
Derric samson <140136414+Derric01@users.noreply.github.com>
2025-01-30 20:22:41 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -10,7 +10,7 @@ Java is a popular programming language, created in 1995.
 
 It is owned by Oracle, and more than 3 billion devices run Java.
 
-It is used for:
+mainly used for:
 
 Mobile applications (specially Android apps)
 Desktop applications
